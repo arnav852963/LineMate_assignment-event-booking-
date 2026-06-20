acc8e8a5a9245b460e44bf05ccda5ad45e254b96
arnav852963 <arnavticku@gmail.com>
2026-06-20 13:48:38 +0530

    added audit logs

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import cookie from 'cookie-parser';
+import {auditLogMiddleware} from "./middlewares/audit.middleware.js";
 
 import { createServer } from 'http';
 import { Server } from 'socket.io';
@@ -33,6 +34,7 @@ app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 app.use(express.static('public'));
 app.use(cookie());
+app.use(auditLogMiddleware);
 
 app.get('/', (req, res) => {
   res.status(200).json({
@@ -50,5 +52,6 @@ app.use((err, req, res, next) => {
 });
 
 import authRoutes from './routes/user.routes.js';
+
 app.use('/api/v1/auth', authRoutes);
 export { httpserver, app, io };
