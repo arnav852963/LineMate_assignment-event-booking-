a11935052529eb8d201999e70ab99e06973c3bd8
arnav852963 <arnavticku@gmail.com>
2026-06-20 09:54:46 +0530

    added user models and authorisation

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,7 @@
+import {ApiError} from "../utilities/ApiError.js";
+import {asyncHandler} from "../utilities/asyncHandler.js";
+import {ApiResponse} from "../utilities/ApiResponse.js";
+
+const register= asyncHandler(async (req,res)=>{
+
+})
---
+++ backend/src/middlewares/jwt_auth.middleware.js
@@ -0,0 +1,22 @@
+import { ApiError } from "../utilities/ApiError.js";
+import jwt from "jsonwebtoken"
+import { User } from "../models/user.model.js";
+import {asyncHandler} from "../utilities/asyncHandler.js";
+
+const jwt_auth =asyncHandler( async (req , res,next )=>{
+    try {
+        const token = req?.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+        if (!token) throw new ApiError(401, "didnt got the token during auth")
+
+        const decodedToken = await jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+        if (!decodedToken) throw new ApiError(401, "didnt got the decodedToken during auth")
+        const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
+        if (!user) throw new ApiError(401, "user didnt fetched during auth")
+        req.user = user
+        next()
+    } catch (e){
+        throw new ApiError(401 , "Please Login")
+    }
+
+})
+export {jwt_auth}
