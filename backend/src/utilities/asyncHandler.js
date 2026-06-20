04ee6615d25f58951e27b58d824922829c8fa9c0
arnav852963 <arnavticku@gmail.com>
2026-06-20 09:26:04 +0530

    completed the setu[p of the backend

---
+++ backend/src/utilities/asyncHandler.js
@@ -0,0 +1,17 @@
+export const asyncHandler = (fun) => {
+  return async (req, res, next) => {
+    try {
+      await fun(req, res, next);
+    } catch (e) {
+      let statusCode = 0;
+      if (e.statusCode >= 100 && e.statusCode <= 1000)
+        statusCode = e.statusCode;
+      else statusCode = 500;
+      res.status(statusCode).json({
+        message: e.message,
+        status: false,
+        statusCode: statusCode,
+      });
+    }
+  };
+};
