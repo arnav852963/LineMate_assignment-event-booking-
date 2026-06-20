e20f90cf560a783bd72ec5216b54be698ca7f2b1
arnav852963 <arnavticku@gmail.com>
2026-06-20 09:31:46 +0530

    completed the setu[p of the backend

---
+++ backend/src/app.js
@@ -0,0 +1,52 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import cookie from 'cookie-parser';
+
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config({ path: './.env' });
+}
+
+const app = express();
+
+const httpserver = createServer(app);
+
+const allowedOrigins = process.env.CORS_ORIGIN;
+const io = new Server(httpserver, {
+  cors: {
+    origin: allowedOrigins,
+    credentials: true,
+  },
+});
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
+
+app.use(express.json({ limit: '16kb' }));
+app.use(express.urlencoded({ extended: true, limit: '16kb' }));
+app.use(express.static('public'));
+app.use(cookie());
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: 'welcome to linemate assignment',
+  });
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.statusCode || 500).json({
+    success: false,
+    message: err.message || 'Internal Server Error',
+    errors: err.errors || [],
+  });
+});
+
+export { httpserver, app, io };
---
+++ backend/src/db/index.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config({
+  path: './.env',
+});
+import { DB_NAME } from '../../constants.js';
+import { ApiError } from '../utilities/ApiError.js';
+
+export const db = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      dbName: DB_NAME,
+    });
+    console.log(`connected to mongodb (db: ${DB_NAME})`);
+  } catch (e) {
+    throw new ApiError(500, `error in mongodb connection -> ${e.message}`);
+  }
+};
