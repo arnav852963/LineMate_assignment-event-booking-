ba244ac4f854b3627ff54e8305057956a292d1d0
arnav852963 <arnavticku@gmail.com>
2026-06-22 09:21:01 +0530

    added ssl

--- backend/.platform/hooks/postdeploy/01_get_certificate.sh
+++ backend/.platform/hooks/postdeploy/01_get_certificate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+# 1. Install Certbot
 sudo dnf install -y python3 augeas-libs
 sudo python3 -m venv /opt/certbot/
 sudo /opt/certbot/bin/pip install --upgrade pip
@@ -8,7 +8,26 @@ sudo /opt/certbot/bin/pip install certbot certbot-nginx
 sudo ln -sf /opt/certbot/bin/certbot /usr/bin/certbot
 
 
-sudo certbot --nginx --non-interactive --agree-tos --email arnavticku@gmail.com -d assignmentlinemate-env.eba-53cfsmfi.ap-southeast-2.elasticbeanstalk.com
+sudo certbot certonly --nginx --non-interactive --agree-tos --email arnavticku@gmail.com -d assignmentlinemate-env.eba-53cfsmfi.ap-southeast-2.elasticbeanstalk.com
+
+
+sudo cat << 'EOF' > /etc/nginx/conf.d/https_custom.conf
+server {
+    listen 443 ssl;
+    server_name assignmentlinemate-env.eba-53cfsmfi.ap-southeast-2.elasticbeanstalk.com;
+
+    ssl_certificate /etc/letsencrypt/live/assignmentlinemate-env.eba-53cfsmfi.ap-southeast-2.elasticbeanstalk.com/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/assignmentlinemate-env.eba-53cfsmfi.ap-southeast-2.elasticbeanstalk.com/privkey.pem;
+
+    location / {
+        proxy_pass http://127.0.0.1:8080;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection "upgrade";
+        proxy_set_header Host $host;
+    }
+}
+EOF
 
 
 sudo systemctl reload nginx
